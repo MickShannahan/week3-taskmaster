44511c937a9f8646fda0f4479123db4c3abb4d6d
MickShannahan <beasticorn@gmail.com>
2020-07-15 16:50:33 -0600

    First Commit

--- app/utils.js
+++ app/utils.js
@@ -16,12 +16,12 @@ export const generateThis = function () {
   let result = '';
   let choices = ["this", "that", "the other", "this one", "this", "this other", "that one", "that other", "the"]
   result = choices[Math.floor(Math.random() * choices.length)]
-  return result + ' list'
+  return result + 'list'
 }
 
 export const generateColor = function () {
   let result = '';
   let choices = ["grey", "dark-grey", "white", "red-grey", "maroon", "grey"]
   result = choices[Math.floor(Math.random() * choices.length)]
-  return result + ' list'
+  return result + 'list'
 }
